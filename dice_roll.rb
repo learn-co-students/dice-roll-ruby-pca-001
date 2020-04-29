31413f02794491d53f91fccb5e4358101dd5e39a
Pat McClernan <patmccler@gmail.com>
2020-04-29 13:22:23 -0400

    update roll to use array

--- dice_roll.rb
+++ dice_roll.rb
@@ -3,5 +3,8 @@
 # Feel free to google "how to generate a random number in ruby"
 
 def roll
-  # code goes here
+  rng = Random.new  
+
+  values = [1,2,3,4,5,6]
+  values[rng.rand(values.size)]
 end
